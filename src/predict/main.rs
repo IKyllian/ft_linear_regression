82b66e78b4400cd6c0e8b9bd31b84b0110865174
IKyllian <kyllian.delporte@gmail.com>
2023-03-14 10:59:44 +0100

    finished predict program

--- src/predict/main.rs
+++ src/predict/main.rs
@@ -1,7 +1,44 @@
 use std::io;
+use csv::Reader;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Thetas {
+    t0: i64,
+    t1: i64,
+}
+
+fn get_thetas() -> Thetas {
+    let rdr = Reader::from_path("thetas.csv");
+    match rdr {
+        Ok(mut f) => {
+            let mut iter = f.deserialize();
+
+            if let Some(result) = iter.next() {
+                match result {
+                    Ok(t) => return t,
+                    Err(_e) => return Thetas { t0: 0, t1: 0 },
+                }
+            } else {
+                Thetas {
+                    t0: 0,
+                    t1: 0,
+                }
+            }
+        },
+        Err(e) => {
+            println!("Error : {}", e);
+            return Thetas { t0: 0, t1: 0 }
+        }
+    }
+}
 
 fn main() {
-    let mileage: u32;
+    let mileage: i64;
+    let thetas: Thetas = get_thetas();
+
+    println!("T0 = {} - T1 = {}", thetas.t0, thetas.t1);
+
     loop {
         let mut input_line = String::new();
 
@@ -13,15 +50,21 @@ fn main() {
 
         match input_line.trim().parse() {
             Ok(num) => {
-                mileage = num;
-                break;
+                if num >= 0 {
+                    mileage = num;
+                    break;
+                } else {
+                    println!("Input must be positive");
+                    continue;
+                }
             },
             Err(_) => {
                 println!("Input not an integer");
                 continue;
-            } 
+            }
         };
     };
 
     println!("Mileage given : {mileage}");
+    println!("Price of a car with {} mileage : {}", mileage, (thetas.t0 + (thetas.t1 * mileage)));
 } 
--- src/training/main.rs
+++ src/training/main.rs
@@ -1,11 +1,27 @@
 use std::error::Error;
 use csv::Reader;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Deserialize)]
 struct Record {
     km: i64,
     price: i64,
+}
+
+#[derive(Serialize)]
+struct Thetas {
+    t0: i64,
+    t1: i64,
+}
+
+fn write_theta_values(t0: i64, t1: i64) -> Result<(), Box<dyn Error>> {
+    let mut writer = csv::Writer::from_path("./thetas.csv")?;
+    writer.serialize(Thetas {
+        t0,
+        t1,
+    })?;
+    writer.flush()?;
+    Ok(())
 }
 
 fn read_csv_file(path: &str) -> Result<(), Box<dyn Error>> {
@@ -19,6 +35,10 @@ fn read_csv_file(path: &str) -> Result<(), Box<dyn Error>> {
 
 fn main(){
     if let Err(e) = read_csv_file("../data.csv") {
+        println!("Error: {}", e)
+    }
+
+    if let Err(e) = write_theta_values(32, 43) {
         println!("Error: {}", e)
     }
 }
